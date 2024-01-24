f661287fadf255f0f0f766b3cddcc5c4306c326e
Anish Sinha <anishsinha0128@gmail.com>
2024-01-24 15:10:42 -0500

    Add plugin to highlight specific types of comments

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,6 +1,8 @@
 require "user.plugins.dotfiles"
 require "user.plugins.themes"
 require "user.plugins.icons"
+require "user.plugins.todo-comments"
+require "user.plugins.trouble"
 
 return {
   colorscheme = "catppuccin-mocha",
