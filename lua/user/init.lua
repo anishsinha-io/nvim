a4574731442269cc82f373f6d1a9b2cc91bc1343
Anish Sinha <anishsinha0128@gmail.com>
2024-01-26 21:52:41 -0500

    Add ocamlformat to conform configuration

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -54,38 +54,16 @@ return {
           -- enable completion triggered by <C-x><C-o>
           vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-          local bufopts = { noremap = true, silent = true, buffer = bufnr }
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-          vim.keymap.set("n", "gk", vim.lsp.buf.signature_help, bufopts)
-          vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-          vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-          vim.keymap.set(
-            "n",
-            "<space>wl",
-            function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-            bufopts
-          )
-          vim.keymap.set("n", "<space>td", vim.lsp.buf.type_definition, bufopts)
-          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-          vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-          vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
-          vim.keymap.set("n", "<space>do", vim.diagnostic.open_float, bufopts)
-          vim.keymap.set("n", "gp", vim.diagnostic.goto_prev, bufopts)
-          vim.keymap.set("n", "gl", vim.diagnostic.goto_next, bufopts)
-          vim.keymap.set("n", "<space>dl", vim.diagnostic.setloclist, bufopts)
-
-          -- format on save
-          if client.server_capabilities.documentFormattingProvider then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = vim.api.nvim_create_augroup("Format", { clear = true }),
-              buffer = bufnr,
-              callback = function() vim.lsp.buf.formatting_seq_sync() end,
-            })
-          end
+          -- TODO: remove the following block (unnecessary since conform is configured with ocamlformat now)
+          --
+          -- if client.server_capabilities.documentFormattingProvider then
+          --   vim.api.nvim_create_autocmd("BufWritePre", {
+          --     group = vim.api.nvim_create_augroup("Format", { clear = true }),
+          --     buffer = bufnr,
+          --     callback = function() vim.lsp.buf.formatting_seq_sync() end,
+          --   })
+          -- end
+          --
           -- code lens
           if client.resolved_capabilities.code_lens then
             local codelens = vim.api.nvim_create_augroup("LSPCodeLens", { clear = true })
--- lua/user/plugins/conform.lua
+++ lua/user/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
       formatters_by_ft = {
         python = { "isort", "black" },
         -- sql = { "sql-formatter" },
+        ocaml = { "ocamlformat" },
 
         ["_"] = { "trim_whitespace" },
       },
