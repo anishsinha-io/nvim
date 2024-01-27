f85181a6de8445891f07a28753db40698644058d
Anish Sinha <anishsinha0128@gmail.com>
2024-01-27 01:54:45 -0500

    Set strict mode to true for dev icon lookup

--- lua/user/plugins/icons.lua
+++ lua/user/plugins/icons.lua
@@ -2,38 +2,48 @@ return {
   {
     "nvim-tree/nvim-web-devicons",
     opts = {
-      override = {
-        -- toml
-        toml = {
-          icon = "",
-          name = "toml",
+      strict = true,
+
+      override_by_filename = {
+        -- FIXME: create an issue on [nvim web devicons](https://github.com/nvim-tree/nvim-web-devicons)
+        -- Cargo.toml override does not work (while opam overrides, not shown below, but tested, do work)
+        -- ["Cargo.toml"] = {
+        --   icon = "󰏓",
+        --   color = "#d7af87",
+        --   cterm_color = "180",
+        --   name = "Cargo",
+        -- },
+        ["test.opam"] = {
+          icon = "",
+          color = "#d75f00",
+          cterm_color = "166",
+          name = "Opam",
         },
-        -- ocaml
-        ml = {
-          icon = "󰘧",
-          color = "#ee6a1a",
-          cterm_color = "130",
-          name = "ocaml",
+        [".ocamlformat"] = {
+          icon = "",
+          name = "OCamlFormat",
         },
         ["dune-project"] = {
           icon = "󰏓",
           color = "#d7af87",
           cterm_color = "180",
-          name = "duneproject",
+          name = "DuneProject",
         },
         dune = {
           icon = "",
-          name = "dune",
+          name = "Dune",
         },
-        [".ocamlformat"] = {
+      },
+      override_by_extension = {
+        ml = {
+          icon = "󰘧",
+          color = "#ee6a1a",
+          cterm_color = "130",
+          name = "OCaml",
+        },
+        toml = {
           icon = "",
-          name = "ocamlformat",
-        },
-        opam = {
-          icon = "",
-          color = "#d75f00",
-          cterm_color = "166",
-          name = "opam",
+          name = "TOML",
         },
       },
     },
