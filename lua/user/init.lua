87f2a28acbc9df8a494c0b5801e9b68836f17359
Anish Sinha <anishsinha0128@gmail.com>
2024-02-23 02:09:17 -0500

    Add NvChad statusline

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -9,8 +9,8 @@ local tmux = require "user.plugins.tmux"
 local autotags = require "user.plugins.autotags"
 local noice = require "user.plugins.noice"
 local overseer = require "user.plugins.overseer"
-
 local tailwind = require "user.plugins.tailwind-rename"
+local heirline = require "user.plugins.heirline"
 
 local mappings = require "user.config.mappings"
 
@@ -19,6 +19,48 @@ return {
   options = {
     opt = {
       showtabline = 0,
+    },
+  },
+
+  icons = {
+    VimIcon = "",
+    ScrollText = "",
+    GitBranch = "",
+    GitAdd = "",
+    GitChange = "",
+    GitDelete = "",
+  },
+
+  heirline = {
+    -- define the separators between each section
+    separators = {
+      left = { "", " " }, -- separator for the left side of the statusline
+      right = { " ", "" }, -- separator for the right side of the statusline
+      tab = { "", "" },
+    },
+    -- add new colors that can be used by heirline
+    colors = function(hl)
+      local get_hlgroup = require("astronvim.utils").get_hlgroup
+      -- use helper function to get highlight group properties
+      local comment_fg = get_hlgroup("Comment").fg
+      hl.git_branch_fg = comment_fg
+      hl.git_added = comment_fg
+      hl.git_changed = comment_fg
+      hl.git_removed = comment_fg
+      hl.blank_bg = get_hlgroup("Folded").fg
+      hl.file_info_bg = get_hlgroup("Visual").bg
+      hl.nav_icon_bg = get_hlgroup("String").fg
+      hl.nav_fg = hl.nav_icon_bg
+      hl.folder_icon_bg = get_hlgroup("Error").fg
+      return hl
+    end,
+    attributes = {
+      mode = { bold = true },
+    },
+    icon_highlights = {
+      file_icon = {
+        statusline = false,
+      },
     },
   },
 
@@ -45,6 +87,7 @@ return {
     tailwind,
     noice,
     overseer,
+    heirline,
   },
 
   lsp = {
